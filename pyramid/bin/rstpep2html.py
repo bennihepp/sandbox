89a4ead0388ff63624434ac9727a64cfa74a8327
Benjamin Hepp <hepp@embl.de>
2011-12-15 14:01:12 +0100

    addition of pyramid virtualenv

---
+++ pyramid/bin/rstpep2html.py
@@ -0,0 +1,5 @@
+#!/g/pepperkok/hepp/code/playground/pyramid/bin/python
+# EASY-INSTALL-SCRIPT: 'docutils==0.8.1','rstpep2html.py'
+__requires__ = 'docutils==0.8.1'
+import pkg_resources
+pkg_resources.run_script('docutils==0.8.1', 'rstpep2html.py')
